ac601cf3069034eddab586985137910eba31cd8e
Renicius Pagotto <rpagotto@avidxchange.com>
2024-08-25 14:19:59 -0300

    First commit

---
+++ iac/version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=1.3.0, < 4.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0, < 4.0.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
